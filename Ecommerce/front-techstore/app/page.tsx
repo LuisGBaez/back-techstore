d7b09c28230cfd3cbe0b69bbc80548d940368cb3
Genaro Baez <lgbaez81@gmail.com>
2025-08-18 19:20:06 -0500

    add shandcn

--- Ecommerce/front-techstore/app/page.tsx
+++ Ecommerce/front-techstore/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
       <h1>Hola Ecommerce Techstore</h1>
+      <Button>Click</Button>
     </div>
   );
 }
