668b55a8599eb73370136a21814d3728b602dda3
Genaro Baez <lgbaez81@gmail.com>
2025-08-20 21:01:12 -0500

    Actualización del backend: 20-08-2025

--- Ecommerce/back-techstore/types/generated/components.d.ts
+++ Ecommerce/back-techstore/types/generated/components.d.ts
@@ -1,3 +1,21 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ProductOrdenItem extends Struct.ComponentSchema {
+  collectionName: 'components_product_orden_items';
+  info: {
+    displayName: 'OrdenItem';
+  };
+  attributes: {
+    price: Schema.Attribute.Decimal & Schema.Attribute.Required;
+    product: Schema.Attribute.Relation<'oneToOne', 'api::product.product'>;
+    quantity: Schema.Attribute.Integer & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'product.orden-item': ProductOrdenItem;
+    }
+  }
+}
